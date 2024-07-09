2fead3d36061862defb526e34c8d191132284a38
eunv0110 <134673146+eunv0110@users.noreply.github.com>
2024-07-09 23:52:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,14 +1,16 @@
 def solution(s):
-    words=s.split(' ')
-    transformed_words = []
-    for word in words:
-        transformed_word = ""
+    s_list=s.split(' ')
+    transformed_words=[]
+    for word in s_list:
+        transformed_word=[]
         for i in range(len(word)):
             if i%2==0:
-                transformed_word+=word[i].upper()
+                transformed_word.append(word[i].upper())
+                
             else:
-                transformed_word+=word[i].lower()
-        transformed_words.append(transformed_word)
-        s=' '.join(transformed_words)
-        
-    return s
+                transformed_word.append(word[i].lower())
+        transformed_words.append(''.join(transformed_word))
+        answer=' '.join(transformed_words)
+    return answer
+            
+            
