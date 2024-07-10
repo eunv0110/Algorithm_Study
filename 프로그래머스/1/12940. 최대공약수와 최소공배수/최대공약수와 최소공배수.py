8170e0020195102079f434dd9614022f46970343
eunv0110 <134673146+eunv0110@users.noreply.github.com>
2024-07-10 22:48:19 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,8 @@
+def solution(n, m):
+    def gcd(a,b):
+        while b!=0:
+            a,b=b,a%b
+        return a
+    gcd_value=gcd(n,m)
+    lcm_value=n*m//gcd_value
+    return (gcd_value,lcm_value)
