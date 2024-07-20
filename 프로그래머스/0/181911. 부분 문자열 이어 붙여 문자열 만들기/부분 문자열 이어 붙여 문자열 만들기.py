599ef7e60f91388c792d848fc98181cf3894167a
eunv0110 <134673146+eunv0110@users.noreply.github.com>
2024-07-20 23:33:39 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -0,0 +1,6 @@
+def solution(my_strings, parts):
+    answer=[]
+    for idx,string in enumerate(my_strings):
+        answer.append(string[parts[idx][0]:parts[idx][1]+1])
+    return ''.join(answer)
+    
