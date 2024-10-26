eb20c90a8ae89c8419eabeb887a275fc81c863d4
eunv0110 <134673146+eunv0110@users.noreply.github.com>
2024-10-27 01:05:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,42 +1,39 @@
-from collections import deque
-
-dx=[0,0,1,-1]
-dy=[1,-1,0,0]
-
-def bfs(graph, a, b):
-    n = len(graph)
-    queue = deque()
-    queue.append((a, b))
-    graph[a][b] = 0
-    count = 1
-
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-                count += 1
-    return count
-
-
-n = int(input())
+N = int(input())
 graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+result = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-cnt = []
-for i in range(n):
-    for j in range(n):
+# 단지 정보 입력 받기
+for _ in range(N):
+    graph.append(list(map(int, input().strip())))
+
+# dfs 함수 정의
+def dfs(x, y):
+    global count
+    graph[x][y] = 0  # 현재 집 방문 처리
+    count += 1  # 현재 위치의 집 개수 증가
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        # 범위를 벗어나거나 집이 없는 경우 무시
+        if nx < 0 or nx >= N or ny < 0 or ny >= N or graph[nx][ny] == 0:
+            continue
+
+        # 연결된 집 탐색
+        dfs(nx, ny)
+
+for i in range(N):
+    for j in range(N):
+        # 새로운 단지를 발견한 경우
         if graph[i][j] == 1:
-            cnt.append(bfs(graph, i, j))
+            count = 0  # 각 단지의 집 개수를 세기 위해 초기화
+            dfs(i, j)
+            result.append(count)  # 단지 내 집의 수 저장
 
-cnt.sort()
-print(len(cnt))
-for i in range(len(cnt)):
-    print(cnt[i])
-    
+# 단지 수와 각 단지의 집 수를 오름차순으로 출력
+print(len(result))
+for count in sorted(result):
+    print(count)
