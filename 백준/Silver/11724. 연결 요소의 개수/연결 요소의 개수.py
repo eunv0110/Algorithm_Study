34771023e2e65022a361480d06a19b943f48cf44
eunv0110 <134673146+eunv0110@users.noreply.github.com>
2024-11-05 23:59:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 588 ms, Memory: 65292 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -3,24 +3,25 @@ sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 
+# dfs 함수
 def dfs(graph, v, visited):
     visited[v] = True
     for i in graph[v]:
         if not visited[i]:
             dfs(graph, i, visited)
 
-n, m = map(int, input().split()) #정점, 간선의 개수
+n, m = map(int, input().split()) # 정점의 개수, 간선의 개수
 graph = [[] for _ in range(n+1)]
 for i in range(m):
     u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
-count = 0
+count = 0 # 연결 노드의 수
 visited = [False] * (n+1)
 for i in range(1, n+1):
     if not visited[i]:
         dfs(graph, i, visited)
-        count += 1 # dfs 한번 끝날때 마다 count+1
+        count += 1 # dfs 한 번 끝날 때마다 count+1
 
 print(count)
