7bbb72dbe4bbe0a19fdc57385c855f8e024c918b
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2024-11-28 23:47:10 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,10 +1,6 @@
 def solution(n):
-    div=1
-    total=[]
-    while n>=div:
-        x=n//div
-        if n%div==0:
-            total.append(x)
-        div+=1
-        
-    return sum(total)
+    answer=[]
+    for i in range(1,n+1):
+        if n%i==0:
+            answer.append(i)
+    return sum(answer)
