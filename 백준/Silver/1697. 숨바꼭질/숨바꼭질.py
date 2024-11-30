a3d97bed4b5055782a3f010915e0c5c626247b6d
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2024-11-30 15:44:22 +0900

    [Silver I] Title: 숨바꼭질, Time: 92 ms, Memory: 35660 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,27 +1,25 @@
 from collections import deque
-import sys
 
-#입력값 받기
-n,k=map(int,sys.stdin.readline().split())
-max_num=100000
-visited=[0]*(max_num+1)
+#수빈이가 있는 위치와 동생이 있는 위치
+n,k=map(int,input().split())
+MAX=100000
+visited=[0]*(MAX+1)
 
-def bfs():
-    q=deque()
-    #출발점 위치 큐에 삽입
-    q.append(n)
-    
-    while q:
-        x=q.popleft()
-        #수빈이 위치가 동생 위치와 같다면 반복문 종료
-        if x==k:
-            print(visited[x])
-            break
-        #이동할 수 있는 방향
-        for i in (x-1,x+1,x*2):
-            #주어진 범위내에 있고 아직 방문하지 않았으면
-            if 0<=i<=max_num and not visited[i]:
-                #이동한 시간 표시
-                visited[i]=visited[x]+1
-                q.append(i)
-bfs()
+def bfs(n):
+  queue=deque([n])
+
+  while queue:
+    x=queue.popleft()
+    #수빈이 위치가 동생 위치와 같다면 종료
+
+    if x==k:
+      return visited[x]
+      
+    for i in (x-1,x+1,x*2):
+      #주어진 범위내에 있고 아직 방문하지 않았다면
+      if 0<=i<=MAX and not visited[i]:
+        #이동한 시간 표시
+        visited[i]=visited[x]+1
+        queue.append(i)
+
+print(bfs(n))
