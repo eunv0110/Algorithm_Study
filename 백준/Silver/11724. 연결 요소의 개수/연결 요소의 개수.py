9d79bf8b397eb5f7ddc1b3e07c7770e3b4ed1bfa
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2024-12-02 21:10:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 564 ms, Memory: 65296 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,31 @@
 import sys
-sys.setrecursionlimit(10**6)
 
+# 재귀 깊이 설정 (예: 10**6)
+sys.setrecursionlimit(10**6)
+# 빠른 입력을 위해 sys.stdin.readline 사용
 input = sys.stdin.readline
 
-# dfs 함수
-def dfs(graph, v, visited):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+n,m=map(int,input().split())
+#그래프 초기화
+graph=[[] for _ in range(n+1)]
+visited=[False]*(n+1)
 
-n, m = map(int, input().split()) # 정점의 개수, 간선의 개수
-graph = [[] for _ in range(n+1)]
-for i in range(m):
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+#간선정보 입력
+for _ in range(m):
+  u,v=map(int,input().split())
+  graph[u].append(v) #문제에서 양방향이라고 했으므로
+  graph[v].append(u)
 
-count = 0 # 연결 노드의 수
-visited = [False] * (n+1)
-for i in range(1, n+1):
+def dfs(v):
+  visited[v]=True #방문처리
+  for i in graph[v]: #방문하지 않은 경우 dfs 재귀적으로 불러오기
     if not visited[i]:
-        dfs(graph, i, visited)
-        count += 1 # dfs 한 번 끝날 때마다 count+1
+      dfs(i)
+
+count=0
+for i in range(1,n+1): #노드는 1번부터 있기 때문에
+  if not visited[i]:
+    dfs(i)
+    count+=1
 
 print(count)
