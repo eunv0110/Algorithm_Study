83fbe198257857014afc03365845ffc10b8c59e1
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2024-12-05 12:17:00 +0900

    [Silver II] Title: 유기농 배추, Time: 228 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,33 +1,48 @@
-import sys
-sys.setrecursionlimit(10**6)#재귀 한도를 크게 설정하여 깊은 재귀 호출을 처리할 수 있도록 설정
+from collections import deque
+#테스트 케이스 횟수 입력받기
+T=int(input())
 
-T = int(input())#테스트 케이스의 수 입력받음
+#방향 선언
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
 
-def dfs(x, y):
-    if x < 0 or y < 0 or x >= M or y >= N: #현재 위치가 범위를 벗어나는지 확인
-        return False
-    if array[x][y] == 1: #현재 위치에 배추가 심어져 있는지 확인
-        array[x][y] = 0 # 방문한 위치를 0으로 바꾸어 표시
-        #상하좌우 인접한 위치를 재귀적으로 방문
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y+1)
-        dfs(x, y-1)
-        return True #새로운 연결된 구성 요소를 찾음
-    return False #현재 위치에 배추가 없거나 이미 방문한 경우
+def bfs(x,y):
+  queue=deque() #큐선언
+  queue.append((x,y)) #시작노드 추가
+  graph[x][y]=0 #방문처리
 
-for i in range(T): #테스트 케이스 반복 처리
-    M, N, K = map(int, input().split())# 격자 크기와 배추의 수 입력
-    array = [[0] * N for i in range(M)] #격자 0으로 초기화 (배추 없음)
+  while queue:
+    x,y=queue.popleft() #노드 꺼내주기
 
-    for i in range(K):#배추의 위치를 읽어서 격자에 표시
-        x, y = map(int, input().split())
-        array[x][y] = 1 #배추의 위치 표시
-    
-    # 각 행 순회
-    total = 0
-    for i in range(M): #각 열 순회
-        for j in range(N): #새로운 배추 그룹 찾으면 1증가
-            if dfs(i, j) == True:
-                total += 1
-    print(total)
+    for i in range(4):
+      nx=dx[i]+x
+      ny=dy[i]+y
+
+      #범위가 맞지 않는 경우 무시 또는 0이면 무시
+      if nx<0 or nx>=M or ny<0 or ny>=N or graph[nx][ny]==0:
+        continue
+      if graph[nx][ny]==1:
+        graph[nx][ny]=0 #방문처리
+        queue.append((nx,ny)) 
+
+results=[]
+
+#배추의 정보에 대한 입력받기
+for _ in range(T): #테스트 케이스 횟수만큼 반복
+  M,N,K=map(int,input().split()) #밭의 가로길이, 세로길이, 위치개수를 다시 입력받음
+  graph=[[0] * N for _ in range(M)] #테스트 테이스마다 그래프를 초기화해줘야하니까
+
+  for _ in range(K):
+    x,y=map(int,input().split())
+    graph[x][y]=1 #배추심기
+
+  count=0
+  for i in range(M):
+    for j in range(N):
+      if graph[i][j]==1:
+        bfs(i,j)
+        count+=1
+  results.append(count)
+
+for result in results:
+  print(result)
