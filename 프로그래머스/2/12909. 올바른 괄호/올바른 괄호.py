e909990fc8627b7d0b772e8477ae89833bd6c39f
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-02-24 10:53:47 +0900

    [level 2] Title: 올바른 괄호, Time: 6.60 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,11 @@
 def solution(s):
     stack=[]
-    
     for char in s:
         if char=='(':
             stack.append(char)
         else:
             if not stack:
                 return False
-            stack.pop() #올바른 경우 짝을 경우
-    return not stack
+            stack.pop()
 
+    return not stack
