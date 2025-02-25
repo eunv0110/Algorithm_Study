49acf16d8e01fec631633167b6aae359a4ed5da4
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-02-26 00:09:49 +0900

    [level 2] Title: 기능개발, Time: 0.49 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,3 @@
 def solution(progresses, speeds):
     answer = []
-    
-    #progresses가 빌때까지
-    while progresses:
-        count=0
-        
-        while progresses and progresses[0]>=100:
-            count+=1
-            speeds.pop(0)
-            progresses.pop(0)
-        
-        for i in range(len(progresses)):
-            progresses[i]=progresses[i]+speeds[i]
-        
-        if count:
-            answer.append(count)
-            
-            
-        
     return answer
