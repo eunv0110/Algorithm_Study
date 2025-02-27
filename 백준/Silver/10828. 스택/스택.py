5c916065a48c1f60b75c2e1542c5d7f76d74f425
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-02-28 01:30:48 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,27 +1,27 @@
 import sys
-
 n = int(sys.stdin.readline())
 
 stack = []
-for i in range(n):
+for _ in range(n):
     command = sys.stdin.readline().split()
-    
-    if command[0] == "push":
-        stack.append(int(command[1]))
-    elif command[0] == "pop":
+
+    if command[0] == 'push':
+        stack.append(command[1])  # push할 때 command[1]에 있는 값을 추가
+
+    elif command[0] == 'pop':
         if stack:
             print(stack.pop())
         else:
-            print(-1)
-    elif command[0] == "size":
+            print(-1)  # 스택이 비어있으면 -1 출력
+
+    elif command[0] == 'size':
         print(len(stack))
-    elif command[0] == "empty":
-        if stack:
-            print(0)
-        else:
-            print(1)
-    elif command[0] == "top":
+
+    elif command[0] == 'empty':
+        print(1 if not stack else 0)  # 삼항연산자로 처리
+
+    elif command[0] == 'top':
         if stack:
             print(stack[-1])
         else:
-            print(-1)
+            print(-1)  # 스택이 비어있으면 -1 출력
