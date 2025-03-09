f06746410392c5cadfbec3c48d79b8b4db574588
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-03-09 18:54:35 +0900

    [level 2] Title: 기능개발, Time: 0.88 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,3 +1,25 @@
+from collections import deque
+
 def solution(progresses, speeds):
-    answer = []
+    
+    queue=deque(progresses)
+    speeds=deque(speeds)
+    answer=[]
+    
+    #queue 빌때까지
+    while queue:
+        count=0
+        for i in range(len(queue)):
+            queue[i]+=speeds[i]
+            
+        #배포가 완료된 항목들
+        #queue가 비고 100% 완료 됐을때
+        while queue and queue[0]>=100:
+            count+=1
+            queue.popleft()
+            speeds.popleft()
+        
+        if count>0:
+            answer.append(count)
+                
     return answer
