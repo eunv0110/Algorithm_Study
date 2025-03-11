28bc43947e4c292ad96932455042c7f6b5586ad4
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-03-11 14:57:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 59.89 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,23 +1,25 @@
 from collections import deque
-
 def solution(bridge_length, weight, truck_weights):
+    #bridge_length:다리에 올라갈 수 있는 트럭수
+    #weight: 다리가 견딜 수 있는 무게
+    #truck_weights:트럭별 무게
+    
     bridge=deque([0]*bridge_length)
-    trucks=deque(truck_weights) #대기트럭
+    truck=deque(truck_weights)
     current_weight=0
     time=0
     
-    #다리에 트럭이 없을때까지
-    while bridge:
-        time+=1
+    while bridge or truck:
         current_weight-=bridge.popleft()
+        time+=1
         
-        #만약에 대기 트럭이 있는 경우
-        if trucks:
-            if current_weight+trucks[0]<=weight:
-                new_truck=trucks.popleft()
+        #대기 트럭이 남아있는 경우
+        if truck:
+            if current_weight+truck[0]<=weight:
+                new_truck=truck.popleft()
                 bridge.append(new_truck)
                 current_weight+=new_truck
             else:
                 bridge.append(0)
-                
+            
     return time
