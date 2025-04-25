30b79a96483ee6321de9291b3f0508df8b763cb9
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-04-25 16:48:00 +0900

    [Silver II] Title: 유기농 배추, Time: 220 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,15 +1,11 @@
 from collections import deque
-#테스트 케이스 횟수 입력받기
-T=int(input())
 
-#방향 선언
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
 def bfs(x,y):
-  queue=deque() #큐선언
-  queue.append((x,y)) #시작노드 추가
-  graph[x][y]=0 #방문처리
+  queue=deque([(x,y)])
+  graph[y][x]=0 #방문처리
 
   while queue:
     x,y=queue.popleft() #노드 꺼내주기
@@ -18,31 +14,32 @@ def bfs(x,y):
       nx=dx[i]+x
       ny=dy[i]+y
 
-      #범위가 맞지 않는 경우 무시 또는 0이면 무시
-      if nx<0 or nx>=M or ny<0 or ny>=N or graph[nx][ny]==0:
+      if nx<0 or nx>=m or ny<0 or ny>=n:
         continue
-      if graph[nx][ny]==1:
-        graph[nx][ny]=0 #방문처리
-        queue.append((nx,ny)) 
+      if graph[ny][nx]==1:
+        graph[ny][nx]=0
+        queue.append((nx,ny))
 
+T=int(input())
 results=[]
 
-#배추의 정보에 대한 입력받기
-for _ in range(T): #테스트 케이스 횟수만큼 반복
-  M,N,K=map(int,input().split()) #밭의 가로길이, 세로길이, 위치개수를 다시 입력받음
-  graph=[[0] * N for _ in range(M)] #테스트 테이스마다 그래프를 초기화해줘야하니까
-
-  for _ in range(K):
+for _ in range(T):
+  m,n,k=map(int,input().split())
+  graph=[[0]*m for _ in range(n)]  
+  for _ in range(k):
     x,y=map(int,input().split())
-    graph[x][y]=1 #배추심기
-
+    graph[y][x]=1
+  
   count=0
-  for i in range(M):
-    for j in range(N):
-      if graph[i][j]==1:
+
+  for i in range(m):
+    for j in range(n):
+      if graph[j][i]==1:
         bfs(i,j)
         count+=1
+
   results.append(count)
 
 for result in results:
   print(result)
+    
