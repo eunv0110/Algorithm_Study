0b4c9405c077108104bb56d1be717a2748ea570f
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-04-25 16:57:01 +0900

    [Silver II] Title: 유기농 배추, Time: 196 ms, Memory: 32776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,24 +1,21 @@
-from collections import deque
-
+import sys
+sys.setrecursionlimit(10**6)#재귀 한도 설정
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-def bfs(x,y):
-  queue=deque([(x,y)])
+def dfs(x,y):
+
   graph[y][x]=0 #방문처리
 
-  while queue:
-    x,y=queue.popleft() #노드 꺼내주기
+  for i in range(4):
+    nx=x+dx[i]
+    ny=y+dy[i]
 
-    for i in range(4):
-      nx=dx[i]+x
-      ny=dy[i]+y
-
-      if nx<0 or nx>=m or ny<0 or ny>=n:
-        continue
-      if graph[ny][nx]==1:
-        graph[ny][nx]=0
-        queue.append((nx,ny))
+    if nx<0 or nx>=m or ny<0 or ny>=n:
+      continue
+    if graph[ny][nx]==1:
+      graph[ny][nx]=0
+      dfs(nx,ny)
 
 T=int(input())
 results=[]
@@ -35,7 +32,7 @@ for _ in range(T):
   for i in range(m):
     for j in range(n):
       if graph[j][i]==1:
-        bfs(i,j)
+        dfs(i,j)
         count+=1
 
   results.append(count)
