3c11c68f66579506141b9020d8016b4327c09940
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-04-29 17:00:17 +0900

    [Silver I] Title: 스타트링크, Time: 412 ms, Memory: 41572 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,22 +1,19 @@
 from collections import deque
 
-F,S,G,U,D=map(int,input().split())
-visited=[0 for _ in range(F+1)]
+f,s,g,u,d=map(int,input().split())
+visited=[False]*(f+1)
 
-def bfs(n):
-  #큐 선언
-  queue=deque([n])
-  visited[n]=1
+def bfs(s):
+  queue=deque([(s,0)])
+  visited[s]=True #방문처리
 
   while queue:
-    x=queue.popleft()
-    
-    if x==G:
-      return visited[x]-1
-    for i in (x+U,x-D):
-      if 1<=i<=F and not visited[i]:
-        queue.append(i)
-        visited[i]=visited[x]+1
+    x,count=queue.popleft()
+    if x==g:
+      return count
+    for i in (x+u,x-d):
+      if 1<=i<=f and not visited[i]:
+        visited[i]=True
+        queue.append((i,count+1))
   return "use the stairs"
-
-print(bfs(S))
+print(bfs(s))
