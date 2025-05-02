19fa58217097dbad4647f7057bcdbcf1b562166a
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-05-02 15:41:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.78 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 16.19 ms
+메모리: 9.31 MB, 시간: 15.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 28일 23:25:06
+2025년 05월 02일 15:41:10
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,37 +1,30 @@
 from collections import deque
-
 def solution(maps):
-    # 방향 설정
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
-    def bfs(x, y):
-        # 큐 선언
-        queue = deque()
-        queue.append((x, y))
+    dx=[-1,1,0,0]
+    dy=[0,0,-1,1]
+    
+    def bfs(x,y):
+        queue=deque([(x,y)])
+        maps[x][y]=1
         
         while queue:
-            x, y = queue.popleft()  # 현재 노드 좌표
+            x,y=queue.popleft()
             
             for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
+                nx=dx[i]+x
+                ny=dy[i]+y
                 
-                # 맵의 범위를 벗어나면 무시하기
-                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]):
+                if nx<0 or nx>=len(maps) or ny<0 or ny>=len(maps[0]) or maps[nx][ny]==0:
                     continue
-                # 벽이면 무시하기
-                if maps[nx][ny] == 0:
-                    continue
-                # 처음 지나가는 길이라면 상하좌우 확인하기
-                if maps[nx][ny] == 1:
-                    maps[nx][ny] = maps[x][y] + 1
-                    queue.append((nx, ny))
-        
-        # 상대 팀 진영까지의 거리 반환
-        return maps[len(maps) - 1][len(maps[0]) - 1]
-
-    # 시작점에서 BFS 실행
-    count = bfs(0, 0)
-    # 도달할 수 없으면 -1 반환
-    return count if count > 1 else -1
+                if maps[nx][ny]==1:
+                    #상하좌우 업데이트
+                    maps[nx][ny]=maps[x][y]+1
+                    queue.append((nx,ny))
+        return maps[len(maps)-1][len(maps[0])-1]
+    
+    #시작점에서 실행
+    answer=bfs(0,0)
+    if answer>1:
+        return answer
+    else:
+        return -1
