e61453ce23e835199b8cdeade1f1fe6437a22e8d
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-05-03 15:11:42 +0900

    [Gold V] Title: 토마토, Time: 1064 ms, Memory: 100436 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,36 +1,33 @@
 from collections import deque
 import sys
 
-m, n = map(int, sys.stdin.readline().strip().split())  # m: 열, n: 행
+m, n = map(int, sys.stdin.readline().strip().split())  # m: 가로, n: 세로
+box = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
 
-box = []
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for _ in range(n):
-    tomato = list(map(int, sys.stdin.readline().strip().split()))
-    box.append(tomato)
-
-queue = deque()
-for i in range(n):
-    for j in range(m):
-        if box[i][j] == 1:
-            queue.append((i, j))
+dx = [-1, 1, 0, 0]  # 좌우
+dy = [0, 0, -1, 1]  # 상하
 
 def bfs():
+    queue = deque()
+
+    for i in range(n):
+        for j in range(m):
+            if box[i][j] == 1:
+                queue.append((i, j))
+
     while queue:
         y, x = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
 
-            if 0 <= ny < n and 0 <= nx < m and box[ny][nx] == 0:
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if 0 <= nx < m and 0 <= ny < n and box[ny][nx] == 0:
                 box[ny][nx] = box[y][x] + 1
                 queue.append((ny, nx))
 
 bfs()
 
-# 결과 계산
 result = 0
 for row in box:
     if 0 in row:
