f6c881da7aa32c4557c07db6831e388be7b759b3
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-05-23 15:56:32 +0900

    [Silver I] Title: 음식물 피하기, Time: 188 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,38 +1,35 @@
 from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
 
+#n-세로, m - 가로, k,음식물 쓰레기 개수
 n,m,k=map(int,input().split())
-graph=[[0]*m for _ in range(n)]
 
-for _ in range(k):
-  r,c=map(int,input().split())
-  graph[r-1][c-1]=1
+graph=[[0]*m for _ in range(n)]
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-def bfs(x,y):
+for _ in range(k):
+  r,c=map(int,input().split())
+  graph[r-1][c-1]=1 #음식물 심기
+
+def dfs(y,x):
+  graph[y][x]=0 #방문처리
   count=1
-  queue=deque([(x,y)])
-  graph[x][y]=0 #방문처리
 
-  while queue:
-    x,y=queue.popleft()
-    for i in range(4):
-      nx=dx[i]+x
-      ny=dy[i]+y
+  for i in range(4):
+    nx=dx[i]+x
+    ny=dy[i]+y
 
-      if nx<0 or nx>=n or ny<0 or ny>=m:
-        continue
-      if graph[nx][ny]==1:
-        count+=1
-        queue.append((nx,ny))
-        graph[nx][ny]=0 #방문처리
+    if 0<=nx<m and 0<=ny<n and graph[ny][nx]==1:
+      count+=dfs(ny,nx)
   return count
 
-result=0
+answer=0
 for i in range(n):
   for j in range(m):
     if graph[i][j]==1:
-      result=max(result,bfs(i,j))
+      answer=max(answer,dfs(i,j))
 
-print(result)
+print(answer)
