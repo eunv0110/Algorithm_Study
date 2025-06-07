d334f536b40955e93b80f4e977087333ac9fb68f
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-07 22:14:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.69 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,8 +2,8 @@ def solution(participant, completion):
     participant.sort()
     completion.sort()
     
-    
-    for p,c in zip(participant,completion):
-        if p!=c:
-            return p
-    return participant[-1]    
+    for i in range(len(participant)-1):
+        if participant[i]!=completion[i]:
+            return participant[i]
+        
+    return participant[-1]
