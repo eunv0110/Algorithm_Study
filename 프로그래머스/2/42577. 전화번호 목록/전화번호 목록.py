a4421cd79e34f4289ba0b75544c0b7634f646462
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-09 14:29:53 +0900

    [level 2] Title: 전화번호 목록, Time: 108.61 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,14 @@
+def solution(phone_book):
+    
+    phone_book.sort()
+        
+    for i in range(len(phone_book)-1):
+        #print(phone_book[i],phone_book[i+1])
+
+        if phone_book[i]==phone_book[i+1][:len(phone_book[i])]:
+            print(phone_book[i],phone_book[i+1][:len(phone_book[i])])
+            return False
+    
+    return True
+        
+    
