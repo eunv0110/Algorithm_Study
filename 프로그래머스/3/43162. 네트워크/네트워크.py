661919d13a3f7659065f986b65af7415422789f1
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-13 14:49:15 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,36 +1,22 @@
-from collections import deque
-
 def solution(n, computers):
-    #방문노드 설정
     
     visited=[False]*n
     count=0
     
     for i in range(n):
         if not visited[i]:
-            print(i,': 새 네트워크 탐색')
-            bfs(i,computers,visited)
+            dfs(i,computers,visited)
             count+=1
+    
+
     return count
 
-def bfs(start, computers,visited):
+def dfs(node,computers,visited):
     
-    queue=deque([start])
-    visited[start]=True #방문처리
-    print(start,"큐에 추가(방문표시)")
+    visited[node]=True #방문처리
     
-    while queue:
-        
-        node=queue.popleft()
-        print(f"노드 {node} 탬색 중, 연결상태 {computers[node]}")
-        
-        for i in range(len(computers[node])):
-                        
-            if computers[node][i]==1 and not visited[i]:
-                print(i,"연결됨!큐에 추가하고 방문표시")
 
-                visited[i]=True #방문처리
-                queue.append(i)
-    
-    
-    
+    for i in range(len(computers)):
+        if computers[node][i]==1 and not visited[i]:
+            dfs(i,computers,visited)
+                
