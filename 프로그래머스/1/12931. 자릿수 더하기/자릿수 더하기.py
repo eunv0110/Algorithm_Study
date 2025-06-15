388f37753151cff62c23542ae35f379e8e4e2bb2
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-15 22:43:15 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,10 +1,5 @@
 def solution(n):
     
-    nums=list(str(n))
-    answer=0
-    
-    for n in nums:
-        answer+=int(n)
-        
+    nums=list(map(int,list(str(n))))
 
-    return answer
+    return sum(nums)
