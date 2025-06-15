f470a340266f7c80e2ac42cd504076ffa00a6334
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-15 14:50:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 716 ms, Memory: 67140 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,31 +1,38 @@
+from collections import deque
 import sys
 
-# 재귀 깊이 설정 (예: 10**6)
-sys.setrecursionlimit(10**6)
-# 빠른 입력을 위해 sys.stdin.readline 사용
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-n,m=map(int,input().split())
-#그래프 초기화
+n,m=map(int,input().strip().split())
+
 graph=[[] for _ in range(n+1)]
 visited=[False]*(n+1)
 
-#간선정보 입력
 for _ in range(m):
   u,v=map(int,input().split())
-  graph[u].append(v) #문제에서 양방향이라고 했으므로
+  graph[u].append(v)
   graph[v].append(u)
 
-def dfs(v):
-  visited[v]=True #방문처리
-  for i in graph[v]: #방문하지 않은 경우 dfs 재귀적으로 불러오기
-    if not visited[i]:
-      dfs(i)
-
 count=0
-for i in range(1,n+1): #노드는 1번부터 있기 때문에
-  if not visited[i]:
-    dfs(i)
+
+def bfs(v):
+  queue=deque([v])
+  visited[v]=True
+
+  while queue:
+
+    v=queue.popleft()
+
+    for i in graph[v]:
+      if not visited[i]:
+        visited[i]=True
+        queue.append(i)
+
+for v in range(1,n+1):
+  if not visited[v]:
+    bfs(v)
     count+=1
 
 print(count)
+
+
