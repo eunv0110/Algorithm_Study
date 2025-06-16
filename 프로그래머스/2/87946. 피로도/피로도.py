52385352e79be9198ee391255858277190aff956
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-16 14:45:18 +0900

    [level 2] Title: 피로도, Time: 35.67 ms, Memory: 14.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,20 +1,3 @@
-from itertools import permutations
 def solution(k, dungeons):
-    
-    answer=[]
-    
-    dungeons_list=list(permutations(dungeons,len(dungeons)))
-    
-    
-    for dungeons in dungeons_list:
-        
-        count=0
-        my_level=k
-
-        for min_level, loss_level in dungeons:
-            
-            if my_level>=min_level:
-                count+=1
-                my_level=my_level-loss_level
-        answer.append(count)
-    return max(answer)
+    answer = -1
+    return answer
