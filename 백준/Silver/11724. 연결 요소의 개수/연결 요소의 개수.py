7038fced5fd4d887ab2c149729945c5dba8872ac
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-23 15:31:28 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 636 ms, Memory: 67072 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -2,37 +2,36 @@ from collections import deque
 import sys
 
 input=sys.stdin.readline
+n,m=map(int,input().split())
 
-n,m=map(int,input().strip().split())
-
-graph=[[] for _ in range(n+1)]
 visited=[False]*(n+1)
 
+graph=[[] for _ in range(n+1)]
+
+#그래프 연결하기
 for _ in range(m):
-  u,v=map(int,input().split())
-  graph[u].append(v)
-  graph[v].append(u)
+  a,b=map(int,input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+def bfs(v):
+  
+  queue=deque([v])
+  visited[v]=True #방문처리
+
+  while queue:
+    x=queue.popleft()
+
+    for i in graph[x]:
+      if not visited[i]:
+        queue.append(i)
+        visited[i]=True
 
 count=0
 
-def bfs(v):
-  queue=deque([v])
-  visited[v]=True
-
-  while queue:
-
-    v=queue.popleft()
-
-    for i in graph[v]:
-      if not visited[i]:
-        visited[i]=True
-        queue.append(i)
-
-for v in range(1,n+1):
-  if not visited[v]:
-    bfs(v)
+for i in range(1,n+1):
+  if not visited[i]:
+    bfs(i)
     count+=1
 
 print(count)
-
-
