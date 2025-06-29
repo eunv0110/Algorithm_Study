a1c5a4574e4016b5c233bf0dc5db63b816d565a5
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-29 21:42:03 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,4 +1,2 @@
 def solution(phone_number):
-    phone_number_end=phone_number[-4:]
-    phnoe_number_star=''.join(list('*' for _ in range(len(phone_number[:-4]))))
-    return phnoe_number_star+phone_number_end
+    return '*'*(len(phone_number)-4)+phone_number[-4:]
