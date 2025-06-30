3c11018c020a6255fea275a53412eb64387e1e06
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-06-30 21:48:13 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.23 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,3 +1,9 @@
 def solution(n):
-    string='수박'*n
-    return string[:n]
+    answer=''
+    for i in range(1,n+1):
+        if i%2==0:
+            answer+='박'
+        else:
+            answer+='수'
+        
+    return answer
