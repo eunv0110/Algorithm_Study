0679431b095f042ef6443d146bccd2d17edf466f
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-07-06 00:14:17 +0900

    [level 2] Title: 피로도, Time: 41.84 ms, Memory: 14.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,3 +1,22 @@
+from itertools import permutations
+
 def solution(k, dungeons):
-    answer = -1
-    return answer
+    cases=list(permutations(dungeons,len(dungeons)))
+    answer=[]
+    for case in cases:
+        #원래 피로도로 리셋
+        need=k
+        count=0
+        
+        for c in case:
+            if need>=c[0]:
+                need-=c[1]
+                count+=1
+            else:
+                break
+        answer.append(count)
+                
+    return max(answer)
+                
+        
+        
