be8fe23a5c9a19f058266036a48671d89d069b52
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-07-07 09:46:07 +0900

    [Silver II] Title: 섬의 개수, Time: 80 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,49 +1,43 @@
 from collections import deque
-#정사각형
-#가로,세로,대각선
 
-dx=[-1,1,0,0,-1,-1,1,1]
-dy=[0,0,-1,1,-1,1,-1,1]
+dx=[-1,1,0,0,-1,1,-1,1]
+dy=[0,0,-1,1,1,1,-1,-1]
 
 def bfs(y,x):
 
-  queue=deque([(y,x)])
-
-  #섬은 1, 바다는 0
   graph[y][x]=0 #방문처리
 
+  queue=deque([(y,x)])
+
   while queue:
+
     y,x=queue.popleft()
 
     for i in range(8):
-      nx=x+dx[i]
-      ny=y+dy[i]
 
+      nx=dx[i]+x
+      ny=dy[i]+y
 
       if 0<=nx<w and 0<=ny<h and graph[ny][nx]==1:
         queue.append((ny,nx))
         graph[ny][nx]=0 #방문처리
 
 while True:
-  
+
   w,h=map(int,input().split())
   graph=[]
-  count=0
 
   if w==0 and h==0:
     break
-  
+
   for _ in range(h):
     graph.append(list(map(int,input().split())))
 
-  for i in range(h):
-    for j in range(w):
-      if graph[i][j]==1:
+  count=0
+
+  for y in range(h):
+    for x in range(w):
+      if graph[y][x]==1:
+        bfs(y,x)
         count+=1
-        bfs(i,j)
-
   print(count)
-
-  
-
-  
