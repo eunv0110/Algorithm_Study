d405e181d3c1dc72ce462e4b3464ec2d62785aa9
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-07-16 23:11:22 +0900

    [level 1] Title: 행렬의 덧셈, Time: 32.74 ms, Memory: 21.9 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,10 +1,9 @@
 def solution(arr1, arr2):
     answer=[]
     for i in range(len(arr1)):
-        answer2=[]
-        for j in range(len(arr1[i])):
-            x=arr1[i][j]+arr2[i][j]
-            answer2.append(x)
-        answer.append(answer2)
+        nums=[]
+        for j in range(len(arr1[0])):
+            nums.append(arr1[i][j]+arr2[i][j])
+        answer.append(nums)
     return answer
-    
+
