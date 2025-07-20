3f875eb4d1fa5c8daa8895f45b3805bfce66c903
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-07-20 21:11:06 +0900

    [level 2] Title: 타겟 넘버, Time: 562.04 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,23 +1,18 @@
 from collections import deque
-
 def solution(numbers, target):
-    queue=deque([(0,0)])
     answer = 0
+    
+    queue=deque([(0,0)])
     
     while queue:
         
         current_num,idx=queue.popleft()
         
-        #모든 숫자를 다 돌았다면
         if idx==len(numbers):
             if current_num==target:
                 answer+=1
-        
-        #그게 아니라면
         else:
             queue.append((current_num+numbers[idx],idx+1))
             queue.append((current_num-numbers[idx],idx+1))
-       
+
     return answer
-        
-    
