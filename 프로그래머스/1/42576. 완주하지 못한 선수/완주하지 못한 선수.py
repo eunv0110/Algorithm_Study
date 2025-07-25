030c89a8d561a22693a304be2623648b5c098c7c
Hwange EunBi <134673146+eunv0110@users.noreply.github.com>
2025-07-25 10:37:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 51.29 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,10 @@
 def solution(participant, completion):
-    
-    answer={}
-    num_sum=0
-    
+    answer = {}
+    p_sum=0
     for p in participant:
-        num=hash(p)
-        answer[num]=p
-        num_sum+=num
+        answer[hash(p)]=(p)
+        p_sum+=hash(p)
     
     for c in completion:
-        num_sum-=hash(c)
-    
-    return answer[num_sum]
+        p_sum-=hash(c)
+    return answer[p_sum]
