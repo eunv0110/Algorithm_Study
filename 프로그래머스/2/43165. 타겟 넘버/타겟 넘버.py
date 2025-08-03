8d0b5a9b391e2511440267fd53d9d791a6c2607b
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-08-03 15:14:09 +0900

    [level 2] Title: 타겟 넘버, Time: 646.97 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,16 @@
 from collections import deque
 
 def solution(numbers, target):
-    answer = 0    
+    
     queue=deque([(0,0)])
+    count=0
     
     while queue:
-        
         current_num,idx=queue.popleft()
-        
         if idx==len(numbers):
             if current_num==target:
-                answer+=1
-        else:
+                count+=1
+        else:    
             queue.append((current_num+numbers[idx],idx+1))
             queue.append((current_num-numbers[idx],idx+1))
-    return answer
+    return count
