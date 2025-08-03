f85e22b1350fb76fba54d9c23599d40744fa7c1b
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-08-03 14:24:52 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,16 +1,13 @@
-from itertools import combinations
 from collections import defaultdict
 
 def solution(clothes):
     
     clothes_dict=defaultdict(list)
+    answer=1
     
     for cloth,kind in clothes:
         clothes_dict[kind].append(cloth)
-        
-    all_case=1
     
     for kind in clothes_dict:
-        all_case*=(len(clothes_dict[kind])+1)
-    
-    return all_case-1
+        answer*=len(clothes_dict[kind])+1
+    return answer-1
