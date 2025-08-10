d8fcb0e5fe54e3d45388cfbc491d5720c90936fe
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-08-10 15:42:01 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.69 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,22 +1,38 @@
+from collections import deque
 def solution(n, wires):
-    def dfs(v,skip_v1,skip_v2):
-        visited[v]=True
-        count=1
-        for i in graph[v]:
-            if (i==skip_v1 and v==skip_v2) or (i==skip_v2 and v==skip_v1):
-                continue
-            if not visited[i]:
-                count+=dfs(i,skip_v1,skip_v2)
-        return count
-    graph=[[] for _ in range(n+1)]
     answer=n
-    
-    for v1,v2 in wires:
-        graph[v1].append(v2)
-        graph[v2].append(v1)
-    for skip_v1,skip_v2 in wires:
-        visited=[False]*(n+1)
-        count=dfs(1,skip_v1,skip_v2)
-        other=n-count
-        answer=min(answer,abs(count-other))
+    for i in range(len(wires)):
+        graph = [[] for _ in range(n+1)]
+        visited=[False]*(n+1) 
+        for idx,(v1,v2) in enumerate(wires):
+            if idx!=i:
+                graph[v1].append(v2)
+                graph[v2].append(v1)       
+        for node in range(1,n+1):
+            if not visited[node]:
+                count=bfs(graph,node,visited)
+                break
+        diff=abs(count-(n-count))
+        answer=min(answer,diff)
+        if answer==0:
+            break
     return answer
+        
+def bfs(graph,start,visited):
+    queue=deque([start])
+    visited[start]=True
+    count=1
+    
+    while queue:
+        
+        node=queue.popleft()
+        
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                queue.append(next_node)
+                visited[next_node]=True
+                count+=1
+    return count
+    
+    
+    
