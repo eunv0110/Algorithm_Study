f85ebb193b69644a4f842dc635d4244e17661ebb
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-08-20 00:01:27 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 68 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,17 +1,15 @@
 from collections import deque
 
-#가로 n, 세로 m
-
+#n 가로크기, m 세로크기
 n,m=map(int,input().split())
-visited=[[False]*n for _ in range(m)]
-graph=[]
 
+graph=[]
+visited=[[False]*n for _ in range(m)]
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
 for _ in range(m):
   graph.append(list(input()))
-
 
 def bfs(y,x,color):
 
@@ -20,28 +18,28 @@ def bfs(y,x,color):
   count=1
 
   while queue:
-
     y,x=queue.popleft()
 
-    for i in range(4):
-      nx=dx[i]+x
-      ny=dy[i]+y
+    for idx in range(4):
+      nx=dx[idx]+x
+      ny=dy[idx]+y
 
-      if 0<=nx<n and 0<=ny<m and not visited[ny][nx]:
-        if graph[ny][nx]==color:
+      if 0<=nx<n and 0<=ny<m:
+        if graph[ny][nx]==color and not visited[ny][nx]:
           queue.append((ny,nx))
           visited[ny][nx]=True
           count+=1
+
   return count
 
+w_sum=0
+b_sum=0
 
-w_count=0
-b_count=0
 for y in range(m):
   for x in range(n):
     if graph[y][x]=='W' and not visited[y][x]:
-      w_count+=bfs(y,x,'W')**2
-    if graph[y][x]=='B' and not visited[y][x]:
-      b_count+=bfs(y,x,'B')**2
+      w_sum+=bfs(y,x,'W')**2
+    elif graph[y][x]=='B' and not visited[y][x]:
+      b_sum+=bfs(y,x,'B')**2
 
-print(w_count,b_count)
+print(w_sum,b_sum)
