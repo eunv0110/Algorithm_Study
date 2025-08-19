fd74154751956288301850e785af8e4acb3d2573
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-08-19 17:45:05 +0900

    [level 3] Title: 네트워크, Time: 0.49 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 0.80 ms
+메모리: 9.22 MB, 시간: 0.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 14일 17:02:27
+2025년 08월 19일 17:45:01
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,27 @@
+from collections import deque
+
 def solution(n, computers):
     visited=[False]*n
-    answer = 0
-
+    answer=0
     for node in range(n):
         if not visited[node]:
-            dfs(node,computers,visited)
+            bfs(node,computers,visited)
             answer+=1
     return answer
 
-def dfs(node, computers,visited):
+def bfs(node,computers,visited):
+    queue=deque([node])
     visited[node]=True
-    for next_node in range(len(computers)):
-        if computers[node][next_node]==1 and not visited[next_node]:
-            dfs(next_node, computers, visited)  # 재귀 호출
+    
+    while queue:
+        
+        node=queue.popleft()
+        
+        for next_node in range(len(computers)):
+            if computers[node][next_node]==1 and not visited[next_node]:
+                visited[next_node]=True
+                queue.append(next_node)
+                
+    
+    
+    
