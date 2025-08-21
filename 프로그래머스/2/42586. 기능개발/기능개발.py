a6ae8acd03681987bf8d50eba1bb3a7fa62a224b
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-08-21 15:36:46 +0900

    [level 2] Title: 기능개발, Time: 0.41 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,14 +1,19 @@
 def solution(progresses, speeds):
     answer=[]
     while progresses:
-        count=0
         for idx in range(len(progresses)):
             progresses[idx]+=speeds[idx]
-        while progresses and progresses[0]>=100:
-            progresses.pop(0)
-            speeds.pop(0) 
-            count+=1
+        count=0  
+        if progresses[0]>=100:
+            while progresses and progresses[0]>=100:
+                progresses.pop(0)
+                speeds.pop(0)
+                count+=1
         if count>0:
             answer.append(count)
     return answer
+                    
         
+        
+    answer = []
+    return answer
