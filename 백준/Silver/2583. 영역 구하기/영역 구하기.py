80f122e0ff6f811556d9754427462a446113b9fa
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-08-30 21:45:20 +0900

    [Silver I] Title: 영역 구하기, Time: 72 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,51 +1,46 @@
 from collections import deque
 
-#m이 세로-> 가로, n이 가로 -> 세로
-m,n,k=map(int,input().split())
-
+#m이 세로, n이 가로
+m,n,k=map(int,input().split(' '))
 
 graph=[[0]*m for _ in range(n)]
 
+#영역 부분
 for _ in range(k):
-  x1,y1,x2,y2=map(int,input().split())
-
-  for x in range(x1,x2):
-    for y in range(y1,y2):
-      graph[x][y]=1 #직사각형 그리기
+  y1,x1,y2,x2=map(int,input().split(' '))
+  for y in range(y1,y2):
+    for x in range(x1,x2):
+      graph[y][x]=1
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-def bfs(x,y):
-  
-  queue=deque([(x,y)])
+def bfs(y,x):
 
-  graph[x][y]=1 #방문처리
-
+  queue=deque([(y,x)])
+  graph[y][x]=1 #방문처리
   count=1
 
   while queue:
-    x,y=queue.popleft()
+    y,x=queue.popleft()
 
     for i in range(4):
       nx=dx[i]+x
       ny=dy[i]+y
 
-      if 0<=nx<n and 0<=ny<m and graph[nx][ny]==0:
-        graph[nx][ny]=1 #방문처리
-        queue.append((nx,ny))
+      if 0<=nx<m and 0<=ny<n and graph[ny][nx]==0:
+        graph[ny][nx]=1
+        queue.append((ny,nx))
         count+=1
   return count
 
-
 results=[]
-
-for i in range(n):
-  for j in range(m):
-    if graph[i][j]==0:
-      results.append(bfs(i,j))
-
+for y in range(n):
+  for x in range(m):
+    if graph[y][x]==0:
+      results.append(bfs(y,x))
+      
 results.sort()
 print(len(results))
-print(*results)
-
+for result in results:
+  print(result,end=' ')
