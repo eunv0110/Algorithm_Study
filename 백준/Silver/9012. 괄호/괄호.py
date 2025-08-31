1331d47a4e41069840f9aa054c83af79620726d8
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-08-31 16:23:58 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,22 +1,21 @@
-import sys
+# 괄호
+T=int(input())
 
-n = int(sys.stdin.readline())
+for _ in range(T):
+  stack=[]
+  parentheses=list(input())
 
-for _ in range(n):
-    answer = []
-    status = True  # 유효성 체크 변수
-    letter = sys.stdin.readline().strip()  # 개행 문자 제거
-
-    for char in letter:
-        if char == '(':
-            answer.append(char)
-        elif char == ')':
-            if not answer:  # 스택이 비어 있으면 짝이 맞지 않음
-                status = False
-                break
-            answer.pop()
-
-    if status and not answer:
-        print("YES")
-    else:
+  for p in parentheses:
+    if p=='(' :
+      stack.append('(')
+    elif p==')':
+      if stack:
+        stack.pop()
+      else:
         print("NO")
+        break
+  else:
+    if stack:
+      print("NO")
+    else:
+      print("YES")
