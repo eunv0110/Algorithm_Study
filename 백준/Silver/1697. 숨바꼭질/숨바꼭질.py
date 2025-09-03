b5128f84a33720699c18ceefd8cabd4c69266754
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-03 14:48:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 36972 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,25 +1,27 @@
 from collections import deque
+import sys
 
-#수빈이가 있는 위치와 동생이 있는 위치
-n,k=map(int,input().split())
-MAX=100000
-visited=[0]*(MAX+1)
+input=sys.stdin.readline
+n,k=map(int,input().split(' '))
+MAX_NUM=100001
+visited=[False]*MAX_NUM
 
 def bfs(n):
-  queue=deque([n])
+  count=0
+  queue=deque([(n,count)])
+  visited[n]=True
 
   while queue:
-    x=queue.popleft()
-    #수빈이 위치가 동생 위치와 같다면 종료
 
-    if x==k:
-      return visited[x]
-      
-    for i in (x-1,x+1,x*2):
-      #주어진 범위내에 있고 아직 방문하지 않았다면
-      if 0<=i<=MAX and not visited[i]:
-        #이동한 시간 표시
-        visited[i]=visited[x]+1
-        queue.append(i)
+    node,count=queue.popleft()
+
+    if node==k:
+      return count
+    
+    else:
+      for i in (node-1,node+1,node*2):
+        if 0<=i<MAX_NUM and not visited[i]:
+          visited[i]=True
+          queue.append((i,count+1))
 
 print(bfs(n))
