63a4600da1332c0211f82e8ee2dd11c13164beaa
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-03 18:44:50 +0900

    [Silver III] Title: 프린터 큐, Time: 60 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,32 +1,32 @@
 from collections import deque
 
-def solution(test_cases):
-    results = []
-    for case in test_cases:
-        N, M, priorities = case
-        queue = deque([(i, p) for i, p in enumerate(priorities)])
-        print_order = 0
+#테스트케이스 수
+T=int(input())
 
-        while queue:
-            current = queue.popleft()
-            if any(current[1] < q[1] for q in queue):
-                queue.append(current)
-            else:
-                print_order += 1
-                if current[0] == M:
-                    results.append(print_order)
-                    break
-    return results
-
-# 입력 처리
-T = int(input())
-test_cases = []
 for _ in range(T):
-    N, M = map(int, input().split())
-    priorities = list(map(int, input().split()))
-    test_cases.append((N, M, priorities))
+  #n: 문서의 개수, m : 몇번째로 놓여있는지 나타내는 정수
+  n,m=map(int,input().split(' '))
 
-# 결과 출력
-results = solution(test_cases)
-for result in results:
-    print(result)
+  #문서 중요도 입력받기
+  importance=list(map(int,list(input().split(' '))))
+  sorted_importance=list(sorted(importance,reverse=True))
+
+  queue=deque()
+  for idx,document in enumerate(importance):
+    queue.append((idx,document))
+  count=0
+
+  while queue:
+    idx,importance=queue.popleft()
+
+    if importance==sorted_importance[0]:
+      sorted_importance.pop(0)
+      count+=1
+
+      if idx==m:
+        print(count)
+        break
+
+    else:
+      queue.append((idx,importance))
+
