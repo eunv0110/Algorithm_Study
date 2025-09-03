d60fd6c82dfcb6cb6f8ab026a56c445225c3e57b
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-03 12:53:16 +0900

    [Silver II] Title: 유기농 배추, Time: 220 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,49 +1,41 @@
 from collections import deque
-#테스트 케이스 T
-T=int(input())
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-
 def bfs(y,x):
-
   queue=deque([(y,x)])
-
   graph[y][x]=0 #방문처리
 
   while queue:
-
     y,x=queue.popleft()
 
     for i in range(4):
-      nx=x+dx[i]
-      ny=y+dy[i]
+      nx=dx[i]+x
+      ny=dy[i]+y
 
       if 0<=nx<m and 0<=ny<n:
         if graph[ny][nx]==1:
-
+          queue.append((ny,nx))
           graph[ny][nx]=0 #방문처리
 
-          queue.append((ny,nx))
+T=int(input())
 
 for _ in range(T):
-  graph=[]
-  count=0
-  #가로길이 m,세로길이 n,배추개수 k
-  m,n,k=map(int,input().split())
-
-  #그래프 생성
+  #가로길이 m, 세로길이 n, 배추가 심어져 있는 위치의 개수 k
+  m,n,k=map(int,input().split(' '))
   graph=[[0]*m for _ in range(n)]
 
   for _ in range(k):
-    x,y=map(int,input().split())
-    graph[y][x]=1 #배추심기
-  
+    x,y=map(int,input().split(' '))
+    graph[y][x]=1
+
+  count=0
+
   for y in range(n):
     for x in range(m):
       if graph[y][x]==1:
         bfs(y,x)
         count+=1
-  
+
   print(count)
