c9af1263b1eca2b3fa5f802b86fa2e95f5a239e0
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-05 15:01:32 +0900

    [Silver I] Title: 음식물 피하기, Time: 180 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,35 +1,37 @@
 from collections import deque
-import sys
-sys.setrecursionlimit(10**6)
 
-#n-세로, m - 가로, k,음식물 쓰레기 개수
+#세로 n, 가로 m, 쓰레기 개수 k
 n,m,k=map(int,input().split())
-
 graph=[[0]*m for _ in range(n)]
+for _ in range(k):
+  r,c=map(int,input().split(' '))
+  graph[r-1][c-1]=1
 
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-for _ in range(k):
-  r,c=map(int,input().split())
-  graph[r-1][c-1]=1 #음식물 심기
-
-def dfs(y,x):
-  graph[y][x]=0 #방문처리
+def bfs(y,x):
+  
+  queue=deque([(y,x)])
+  graph[y][x]=0
   count=1
 
-  for i in range(4):
-    nx=dx[i]+x
-    ny=dy[i]+y
+  while queue:
+    y,x=queue.popleft()
 
-    if 0<=nx<m and 0<=ny<n and graph[ny][nx]==1:
-      count+=dfs(ny,nx)
+    for i in range(4):
+      nx=dx[i]+x
+      ny=dy[i]+y
+
+      if 0<=nx<m and 0<=ny<n and graph[ny][nx]==1:
+        queue.append((ny,nx))
+        graph[ny][nx]=0
+        count+=1
   return count
-
 answer=0
-for i in range(n):
-  for j in range(m):
-    if graph[i][j]==1:
-      answer=max(answer,dfs(i,j))
+for y in range(n):
+  for x in range(m):
+    if graph[y][x]==1:
+      answer=max(answer,bfs(y,x))
 
 print(answer)
