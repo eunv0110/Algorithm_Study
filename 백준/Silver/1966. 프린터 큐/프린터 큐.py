8cb0d97e3c8db7ac7fb54750aecbc4c2f9cce014
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-08 16:21:33 +0900

    [Silver III] Title: 프린터 큐, Time: 60 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,30 +1,31 @@
 from collections import deque
 
-#테스트할 개수
 T=int(input())
 
 for _ in range(T):
-  #문서의 개수 n, 놓여져있는 정수 m
+  #문서의 개수 n , 궁금한 문서 m
   n,m=map(int,input().split(' '))
+  queue=deque()
+
   #중요도
   importances=list(map(int,input().split(' ')))
-  sorted_importances=deque(sorted(importances,reverse=True))
-  documents=deque()
+
+  #중요도 정렬
+  sorted_importances=deque(list(sorted(importances,reverse=True)))
+
   for idx,importance in enumerate(importances):
-    documents.append((idx,importance))
+    queue.append((idx,importance))
 
   count=0
 
-  while documents:
-    idx,importance=documents.popleft()
+  while queue:
+    idx,importance=queue.popleft()
 
     if importance>=sorted_importances[0]:
       sorted_importances.popleft()
       count+=1
-
       if idx==m:
         print(count)
         break
-
     else:
-      documents.append((idx,importance))    
+      queue.append((idx,importance))
