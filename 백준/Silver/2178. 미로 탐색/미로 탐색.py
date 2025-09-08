933317713736c52a9b9d2add03e1acaf56acd460
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-08 15:03:19 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -4,27 +4,24 @@ n,m=map(int,input().split(' '))
 graph=[]
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
+
 for _ in range(n):
-  info=list(input())
-  graph.append(list(map(int,info)))
+  graph.append(list(map(int,list(input()))))
 
 def bfs(y,x):
-
   queue=deque([(y,x)])
 
   while queue:
-
     y,x=queue.popleft()
 
     for i in range(4):
-      nx=dx[i]+x
-      ny=dy[i]+y
+      nx=x+dx[i]
+      ny=y+dy[i]
 
-      if 0<=nx<m and 0<=ny<n and graph[ny][nx]==1:
-        graph[ny][nx]=graph[y][x]+1
-        queue.append((ny,nx))
-      
+      if 0<=nx<m and 0<=ny<n:
+        if graph[ny][nx]==1:
+          queue.append((ny,nx))
+          graph[ny][nx]=graph[y][x]+1
   return graph[-1][-1]
-
 
 print(bfs(0,0))
