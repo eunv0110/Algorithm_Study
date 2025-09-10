6b989adcfbfeeeccc9b02834a49c2311d91cf74c
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-10 10:38:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 60 ms, Memory: 34916 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -4,9 +4,8 @@ n,k=map(int,input().split(' '))
 
 queue=deque([i for i in range(1,n+1)])
 answer=[]
-
 while queue:
   queue.rotate(-(k-1))
   answer.append(queue.popleft())
 
-print("<"+', '.join(map(str,answer))+">")
+print('<'+', '.join(map(str,answer))+'>')
