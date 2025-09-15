def5fc51ce4a5c09be8e3bfd9c3255b87cb886db
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-15 18:57:43 +0900

    [Silver II] Title: N과 M (9), Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,27 +1,22 @@
-import sys
-input=sys.stdin.readline
-
 n,m=map(int,input().split(' '))
 nums=list(map(int,input().split(' ')))
 nums.sort()
-
 visited=[False]*n
 results=[]
 
-def recur(depth):
-  if depth==m:
+def recur(num):
+  if num==m:
     print(' '.join(map(str,results)))
     return
-
+  
   used=set()
 
   for i in range(n):
     if not visited[i] and nums[i] not in used:
-      visited[i]=True #방문처리
+      visited[i]=True
       results.append(nums[i])
-      used.add(nums[i]) #이번 depth에서 중복 방지
-      recur(depth+1)
-      visited[i]=False
+      used.add(nums[i])
+      recur(num+1)
       results.pop()
-
+      visited[i]=False
 recur(0)
