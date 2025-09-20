22dcbe9104ed5049bbaf72baeda76f0dd58728fc
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-20 17:28:29 +0900

    [Silver II] Title: N과 M (9), Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -8,15 +8,14 @@ def recur(num):
   if num==m:
     print(' '.join(map(str,results)))
     return
-  
   used=set()
-
   for i in range(n):
     if not visited[i] and nums[i] not in used:
       visited[i]=True
       results.append(nums[i])
       used.add(nums[i])
       recur(num+1)
-      results.pop()
       visited[i]=False
+      results.pop()
+
 recur(0)
