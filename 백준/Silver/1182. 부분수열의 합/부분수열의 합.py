f39e5ffb854a6a290efcbcdd4728b3d84f340f96
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-09-22 12:13:13 +0900

    [Silver II] Title: 부분수열의 합, Time: 204 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,17 +1,16 @@
 n,s=map(int,input().split(' '))
 nums=list(map(int,input().split(' ')))
-results=[]
 count=0
-def recur(idx,current_sum):
+
+def recur(idx,total):
   global count
   if idx==n:
-    if current_sum==s:
+    if total==s:
       count+=1
     return
 
-  recur(idx+1,current_sum)
-  recur(idx+1,current_sum+nums[idx])
-
+  recur(idx+1,total+nums[idx])
+  recur(idx+1,total)
 recur(0,0)
 if s==0:
   print(count-1)
