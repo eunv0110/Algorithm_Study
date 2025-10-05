89f4e2a81272405d9b4929ac6e9a66501d15c9b6
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-05 22:32:15 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,56 +1,46 @@
 from collections import deque
-
 n=int(input())
 graph=[]
-visited=[[False]*n for _ in range(n)]
-visited2=[[False]*n for _ in range(n)]
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
 
 for _ in range(n):
   graph.append(list(input()))
 
-graph2=[row[:] for row in graph]
+color_graph=[row[:] for row in graph]
 
 for y in range(n):
   for x in range(n):
-    if graph2[y][x]=='G':
-      graph2[y][x]='R'
+    if color_graph[y][x]=='G':
+      color_graph[y][x]='R'
 
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
 
 def bfs(y,x,color,graph,visited):
 
   queue=deque([(y,x)])
-  visited[y][x]=True #방문처리
+  visited[y][x]=True
 
   while queue:
-
     y,x=queue.popleft()
 
     for i in range(4):
-      nx=dx[i]+x
-      ny=dy[i]+y
+      nx=x+dx[i]
+      ny=y+dy[i]
 
-      if 0<=nx<n and 0<=ny<n and not visited[ny][nx]:
+      if 0<=nx<n and 0<=ny<n:
         if graph[ny][nx]==color:
-          queue.append((ny,nx))
-          visited[ny][nx]=True
+          if not visited[ny][nx]:
+            queue.append((ny,nx))
+            visited[ny][nx]=True
 
-count=0
-c_count=0
+def count_area(graph):
+  visited=[[False]*n for _ in range(n)]
+  count=0
+  for y in range(n):
+    for x in range(n):
+      if not visited[y][x]:
+        bfs(y,x,graph[y][x],graph,visited)
+        count+=1
+  return count
 
-for y in range(n):
-  for x in range(n):
-    if not visited[y][x]:
-      bfs(y,x,graph[y][x],graph,visited)
-      count+=1
-
-
-for y in range(n):
-  for x in range(n):
-    if not visited2[y][x]:
-      bfs(y,x,graph2[y][x],graph2,visited2)
-      c_count+=1
-print(count,c_count)
-
-
+print(count_area(graph),count_area(color_graph))
