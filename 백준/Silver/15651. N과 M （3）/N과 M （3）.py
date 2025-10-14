e7ebb7444c92c1e7e33bbf03abb34151902eb6df
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-14 16:45:35 +0900

    [Silver III] Title: N과 M (3), Time: 1104 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,15 +1,11 @@
-
 n,m=map(int,input().split(' '))
 results=[]
-
 def recur(depth):
   if depth==m:
     print(' '.join(map(str,results)))
     return
-  
   for i in range(1,n+1):
     results.append(i)
     recur(depth+1)
     results.pop()
-
 recur(0)
