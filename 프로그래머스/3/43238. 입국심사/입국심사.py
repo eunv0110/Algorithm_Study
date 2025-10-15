849a88864700e49362a15488ef2ec0270f2f2c89
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-15 17:16:06 +0900

    [level 3] Title: 입국심사, Time: 698.63 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,12 +1,14 @@
 def solution(n, times):
-    start=1
+    start = 1
     end=max(times)*n
-    answer = 0
-
+    answer=0
     while start<=end:
+        total=0
+        
+        if start>end:
+            break
         
         mid=(start+end)//2
-        total=0
         
         for time in times:
             total+=mid//time
@@ -16,5 +18,6 @@ def solution(n, times):
             end=mid-1
         else:
             start=mid+1
+        
     
     return answer
