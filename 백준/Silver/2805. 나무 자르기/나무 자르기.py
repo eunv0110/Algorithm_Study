26ccc9462025ba9c845bb4627266febe2520a5ac
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-15 15:56:49 +0900

    [Silver II] Title: 나무 자르기, Time: 2920 ms, Memory: 145580 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,27 @@
-#나무의 수 : n, 상근이가 집으로 가져가려고 하는 나무의 길이 m
 n,m=map(int,input().split(' '))
-trees=list(map(int,input().split(' ')))
+nums=list(map(int,input().split(' ')))
 start=0
-end=max(trees)
-result = 0
+end=max(nums)
+answer=0
 
-while start<=end:
-  mid=(start+end)//2
-  
+def find_height(start,end,answer):
   total=0
-   
-  for tree in trees:
-    if tree>mid:
-      total+=tree-mid
-  if total>=m:
-    result=mid
-    start=mid+1
-  else:
-    end=mid-1
+  if start>end:
+    return answer
 
-print(result)
+  mid=(start+end)//2
+
+  for num in nums:
+    total+=max(0,num-mid)
+
+  if total>=m:
+    answer=mid
+    return find_height(mid+1,end,answer)
+
+  else:
+    return find_height(start,mid-1,answer)
+
+
+answer=find_height(start,end,answer)
+print(answer)
+
