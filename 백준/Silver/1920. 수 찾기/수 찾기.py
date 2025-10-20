8a0cddc1ba3a9effb0c28441cc0fa85e1dd5bbd1
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-20 12:26:36 +0900

    [Silver IV] Title: 수 찾기, Time: 228 ms, Memory: 51044 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,13 +1,12 @@
-from bisect import bisect_left
+from bisect import bisect_right,bisect_left
 
-n = int(input())
-arr = sorted(list(map(int, input().split())))  # ✅ 정렬 필수
-m = int(input())
-nums = list(map(int, input().split()))
-
+n=int(input())
+arr=list(sorted(map(int,input().split(' '))))
+m=int(input())
+nums=list(map(int,input().split(' ')))
 for num in nums:
-    idx = bisect_left(arr, num)
-    if idx < n and arr[idx] == num:
-        print(1)
-    else:
-        print(0)
+  idx=bisect_left(arr,num)
+  if idx < n and arr[idx] == num:
+    print(1)
+  else:
+    print(0)
