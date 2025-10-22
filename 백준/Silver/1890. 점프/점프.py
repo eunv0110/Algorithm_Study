95e6418e1285afa39663874d252e40a24aeb581d
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-22 18:46:47 +0900

    [Silver I] Title: 점프, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,17 @@
+n=int(input())
+graph=[list(map(int,input().split(' ')))for _ in range(n)]
+dp=[[0]*n for _ in range(n)]
+dp[0][0]=1
+
+for i in range(n):
+  for j in range(n):
+    if dp[i][j]==0 or (i == n-1 and j == n-1):
+      continue
+    step=graph[i][j]
+
+    if i+step<n:
+      dp[i+step][j]+=dp[i][j]
+    if j+step<n:
+      dp[i][j+step]+=dp[i][j]
+
+print(dp[n-1][n-1])
