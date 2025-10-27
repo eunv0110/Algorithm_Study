2d027f4261f87a12cef9b9b48788dc72bd001a4c
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-27 18:28:44 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,9 @@
 n=int(input())
-p=list(reversed(sorted(map(int,input().split(' ')))))
+p=list(sorted(map(int,input().split(' '))))
 time=0
 total=0
-for _ in range(n):
-  time+=p.pop()
+for i in p:
+  time+=i
   total+=time
 
 print(total)
