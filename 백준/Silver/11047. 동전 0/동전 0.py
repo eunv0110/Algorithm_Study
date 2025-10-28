886ece615a7a39c8008618595eedbc02b248976c
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-28 12:42:34 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,19 +1,13 @@
-import sys
-
-n,k=map(int,sys.stdin.readline().split())
-coins=[]
-result=0
-i=0
-
-for _ in range(n):
-  coin=int(sys.stdin.readline().strip())
-  if k>=coin:
-    coins.append(coin)
+n,k=map(int,input().split(' '))
+coins=[int(input()) for _ in range(n)]
 coins.reverse()
-
-while i<len(coins):
-  result+=k//coins[i]
-  k=k%coins[i]
-  i+=1
-
-print(result)
+answer=0
+for coin in coins:
+  count=0
+  if k==0:
+    break
+  if k>=coin:
+    count=k//coin
+    answer+=count
+    k=k-count*coin
+print(answer)
