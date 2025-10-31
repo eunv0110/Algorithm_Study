19e34793394bed2c369cb33b214d4c0ca4e09a1b
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-10-31 14:30:41 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,13 +1,16 @@
 n,m=map(int,input().split(' '))
 results=[]
+visited=[False]*(n+1)
 
 def recur(start):
+
   if len(results)==m:
     print(' '.join(map(str,results)))
     return
   
   for i in range(start,n+1):
-    results.append(i)
-    recur(i+1)
-    results.pop()
+    if not visited[i]:
+      results.append(i)
+      recur(i+1)
+      results.pop()
 recur(1)
