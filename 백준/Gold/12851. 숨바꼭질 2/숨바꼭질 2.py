815a250ba3ce9493d41a7893bce18e251674eaa8
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-11-02 23:06:32 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 104 ms, Memory: 38776 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -2,14 +2,14 @@ from collections import deque
 
 n,k=map(int,input().split(' '))
 MAX_NUM=100001
-ways=[0]*MAX_NUM
 dist=[-1]*MAX_NUM
+ways=[0]*MAX_NUM
 
 def bfs(n):
 
+  dist[n]=0
+  ways[n]=1
   queue=deque([n])
-  dist[n]=0 #방문처리
-  ways[n]=1 #경우의 수 업데이트
 
   while queue:
 
@@ -17,16 +17,17 @@ def bfs(n):
 
     for nx in (x-1,x+1,2*x):
       if 0<=nx<MAX_NUM:
-        #방문한적이 없다면
+        #한번도 방문하지 않은 경우
         if dist[nx]==-1:
-          dist[nx]=dist[x]+1
           ways[nx]=ways[x]
+          dist[nx]=dist[x]+1
           queue.append(nx)
-        
-        #최소 길이로 방문한적이 있다면
+        #방문한적이 있는 경우
         elif dist[nx]==dist[x]+1:
           ways[nx]+=ways[x]
 
 bfs(n)
 print(dist[k])
-print(ways[k])
+print(ways[k])      
+      
+        
