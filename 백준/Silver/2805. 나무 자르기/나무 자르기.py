2016dda7591e016e7ce525584843ff74ba2b3541
EunBi <134673146+eunv0110@users.noreply.github.com>
2025-11-20 12:56:20 +0900

    [Silver II] Title: 나무 자르기, Time: 2236 ms, Memory: 143532 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,28 @@
+# n : 나무의 수, m: 상근이가 원하는 나무의 수
 n,m=map(int,input().split(' '))
 trees=list(map(int,input().split(' ')))
 start=0
 end=max(trees)
 answer=0
 def recur(start,end,answer):
+  
+  height=0
   if start>end:
     return answer
-
+  
   mid=(start+end)//2
 
-  total=0
   for tree in trees:
-    total+=max(0,tree-mid)
+    if tree>mid:
+      height+=tree-mid
   
-  if total>=m:
-    return recur(mid+1,end,mid)
-  else:
+  #자른 양이 부족 : 더 낮게 잘라야함
+  if m>height:
     return recur(start,mid-1,answer)
+    
+  #자른 양이 충분 : 더 높게 잘라보기
+  else:
+    answer=mid
+    return recur(mid+1,end,mid)
+
 print(recur(start,end,answer))
-
-
